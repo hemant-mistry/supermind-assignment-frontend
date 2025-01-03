ca1a45b9dec0d4e7767bb8311bb5561ca2728f29
hemant-mistry <108870271+hemant-mistry@users.noreply.github.com>
2025-01-04 03:40:58 +0530

    updated the flask api with health-check endpoint as well as dependencies

--- backend/app.py
+++ backend/app.py
@@ -8,6 +8,9 @@ def get_response():
 
     print(langflowsetup.start_flow())
     
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
 
 if __name__ == "__main__":
     app.run(debug=True)
