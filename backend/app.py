47da9c05f9a055fffe613b369cdbacfbff13bb69
hemant-mistry <108870271+hemant-mistry@users.noreply.github.com>
2025-01-05 02:12:48 +0530

    configuring cors to accept requests from frontend

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,9 @@
 from flask import Flask, request, jsonify
 import langflowsetup
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route("/api/getresponse", methods = ['POST'])
 def get_response():
